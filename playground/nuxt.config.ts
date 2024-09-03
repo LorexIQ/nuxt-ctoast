0d5720f0a3c52370f6e27d71fb80172978ef2193
LorexIQ <i@liq-mail.ru>
2024-09-03 17:57:13 +0300

    fix: types for new nuxt version

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,7 +1,12 @@
 export default defineNuxtConfig({
-  modules: ['../src/module'],
+  modules: [
+    '../dist',
+    //'nuxt-ctoast'
+  ],
 
   devtools: {
     enabled: true,
   },
+
+  compatibilityDate: '2024-04-03',
 });
